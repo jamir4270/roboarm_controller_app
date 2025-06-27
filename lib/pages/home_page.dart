192ad69438106cd2948886f0af2e22723eb28aa3
Jamir Oasis Andrade <jamirandrade4270@gmail.com>
2025-06-27 23:46:48 +0800

    [feature] added bluetooth scanner

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
+import "dart:async";
+
+import "package:flutter/foundation.dart";
 import "package:flutter/material.dart";
-import "package:bluetooth_classic/bluetooth_classic.dart";
-import 'package:bluetooth_classic/models/device.dart';
+import 'package:flutter_blue_classic/flutter_blue_classic.dart';
 import "package:roboarm_controller_app/components/instructions_box.dart";
 import "package:roboarm_controller_app/components/my_joystick.dart";
 
@@ -13,8 +15,136 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   bool deviceStatus = false;
+  final _blueClassicPlugin = FlutterBlueClassic(usesFineLocation: true);
+
+  BluetoothAdapterState _adapterState = BluetoothAdapterState.unknown;
+  StreamSubscription? _adapterStateSubscription;
+
+  final Set<BluetoothDevice> _scanResults = {};
+  StreamSubscription? _scanSubscription;
+
+  bool _isScanning = false;
+  int? _connectingToIndex;
+  StreamSubscription? _scanningStateSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  Future<void> initPlatformState() async {
+    BluetoothAdapterState adapterState = _adapterState;
+
+    try {
+      adapterState = await _blueClassicPlugin.adapterStateNow;
+      _adapterStateSubscription = _blueClassicPlugin.adapterState.listen((
+        current,
+      ) {
+        if (mounted) setState(() => _adapterState = current);
+      });
+      _scanSubscription = _blueClassicPlugin.scanResults.listen((device) {
+        if (mounted) setState(() => _scanResults.add(device));
+      });
+      _scanningStateSubscription = _blueClassicPlugin.isScanning.listen((
+        isScanning,
+      ) {
+        if (mounted) setState(() => _isScanning = _isScanning);
+      });
+    } catch (e) {
+      if (kDebugMode) print(e);
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      _adapterState = adapterState;
+    });
+  }
+
+  @override
+  void dispose() {
+    _adapterStateSubscription?.cancel();
+    _scanSubscription?.cancel();
+    _scanningStateSubscription?.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    List<BluetoothDevice> scanResults = _scanResults.toList();
+
+    for (int i = 0; i < scanResults.length; i++) {
+      print(scanResults[i].name);
+    }
+
+    void displayDevices() {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("Available Devices"),
+            content: SizedBox(
+              width: 300.0,
+              child: ListView(
+                children: [
+                  SizedBox(),
+                  if (scanResults.isEmpty)
+                    const Center(child: Text("No devices found yet"))
+                  else
+                    for (var (index, result) in scanResults.indexed)
+                      ListTile(
+                        title: Text(
+                          "${result.name ?? "???"} (${result.address})",
+                        ),
+                        trailing: index == _connectingToIndex
+                            ? const CircularProgressIndicator()
+                            : Text("${result.rssi} dBm"),
+                        onTap: () async {
+                          BluetoothConnection? connection;
+                          setState(() => _connectingToIndex = index);
+                          try {
+                            connection = await _blueClassicPlugin.connect(
+                              result.address,
+                            );
+                            if (!this.mounted) return;
+                            if (connection != null && connection.isConnected) {
+                              if (mounted) {
+                                setState(() => _connectingToIndex = null);
+                              }
+                            }
+                          } catch (e) {
+                            if (mounted) {
+                              setState(() => _connectingToIndex = null);
+                            }
+                            if (kDebugMode) print(e);
+                            connection?.dispose();
+                            ScaffoldMessenger.maybeOf(context)?.showSnackBar(
+                              const SnackBar(
+                                content: Text("Error connecting to device"),
+                              ),
+                            );
+                          }
+                        },
+                      ),
+                ],
+              ),
+            ),
+            actions: [
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    _blueClassicPlugin.stopScan();
+                  },
+                  child: Text("Stop"),
+                ),
+              ),
+            ],
+          );
+        },
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(title: Text("RoboArm Controller")),
       body: Column(
@@ -22,9 +152,16 @@ class _HomePageState extends State<HomePage> {
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(deviceStatus ? "Connected" : "Not Connected"),
+              Text("Adapter State: " + _adapterState.name),
               SizedBox(width: 20),
-              ElevatedButton(onPressed: () {}, child: Text("Scan RoboArm")),
+              ElevatedButton(
+                onPressed: () {
+                  _blueClassicPlugin.turnOn();
+                  _blueClassicPlugin.startScan();
+                  displayDevices();
+                },
+                child: Text("Scan RoboArm"),
+              ),
             ],
           ),
           SizedBox(height: 30),
